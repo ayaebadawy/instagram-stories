53a8e0c6ff6dcca41265c515b40cdbcf9d63bf24
aembadawy <aem.badawy@gmail.com>
2025-06-16 11:39:23 +0300

    Initial Commit

---
+++ Instagram-stories/Instagram_storiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_storiesApp.swift
+//  Instagram-stories
+//
+//  Created by Aya on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_storiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
